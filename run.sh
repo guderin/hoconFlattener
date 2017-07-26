9b8758e11ffba7880d45649be4fcd02f7725b83d
guderin <guderin@gmail.com>
2017-07-26 17:15:22 +0200

    Next step

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CONFIG_FILE_NAME=$1
 APP_NAME=$(find target -name *jar-with-dependencies.jar)
 
-java -jar ${APP_NAME}
+java -jar ${APP_NAME} ${CONFIG_FILE_NAME}
--- src/main/java/pl/guderin/Main.java
+++ src/main/java/pl/guderin/Main.java
@@ -1,5 +1,9 @@
 package pl.guderin;
 
+import com.typesafe.config.*;
+import java.io.File;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +12,25 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String configFileName = args[0];
+        System.out.println("configFileName: " + configFileName);
+
+        File configFile = new File(configFileName);
+
+        Config conf = ConfigFactory.parseFile(configFile).resolve();
+
+        ConfigRenderOptions options = ConfigRenderOptions.defaults()
+                .setFormatted(true)
+                .setComments(true)
+                .setOriginComments(false);
+
+
+        //conf.checkValid(ConfigFactory.defaultReference());
+
+        System.out.println(conf.root().render(options));
+
+//        System.out.println("The answer is: " + conf.getString("simple-lib.foo"));
+
+
     }
 }
